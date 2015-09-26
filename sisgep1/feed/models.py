caec21146526d6c0aa949d099bfff8d1ce9e2a41
Arthur Bressan <arthurpbressan@gmail.com>
2015-09-26 12:55:25 -0300

    Os modelos tem que ter campos específicos

--- sisgep1/feed/models.py
+++ sisgep1/feed/models.py
@@ -31,15 +31,15 @@ class Role(db.Model, RoleMixin, JSONSerializationMixin):
     ignored_fields = ['usuarios']
 
     id = Column(Integer(), primary_key=True)
-    nome = Column(String(80), unique=True)
-    descricao = Column(String(255))
+    name = Column(String(80), unique=True)
+    description = Column(String(255))
 
 
 class Usuario(db.Model, UserMixin, JSONSerializationMixin):
     id = Column(Integer, primary_key=True)
     email = Column(String(255), unique=True)
-    senha = Column(String(255))
-    ativo = Column(Boolean())
-    confirmado = Column(DateTime())
+    password = Column(String(255))
+    active = Column(Boolean())
+    confirmed_at = Column(DateTime())
     roles = relationship('Role', secondary=roles_users,
                          backref=backref('usuarios', lazy='dynamic'))
