98cd22ef5429004f9cff50bccf41b2c13b77858f
Arthur Bressan <arthurpbressan@gmail.com>
2015-09-26 11:58:11 -0300

    Adiciona plataformas sociais

--- settings.py
+++ settings.py
@@ -4,3 +4,13 @@ from __future__ import absolute_import
 DEBUG = True
 
 SQLALCHEMY_DATABASE_URI = 'sqlite:////tmp/sisgep1.db'
+
+
+SOCIAL_FACEBOOK = {
+    'consumer_key': 'key',
+    'consumer_secret': 'secret'
+}
+SOCIAL_TWITTER = {
+    'consumer_key': 'key',
+    'consumer_secret': 'secret'
+}
